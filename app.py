b516bdef3c16122347be8ac5675801d915247979
Сергей Машошин <darvin212@gmail.com>
2026-06-25 02:16:15 +0300

    Updated README.md

--- app.py
+++ app.py
@@ -376,4 +376,4 @@ def get_active_assignments_api(team_id: int, start_date: Optional[str] = None, e
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=5000)
+    uvicorn.run(app, port=5000)
